1f5de60ef660da7a272007af1ac24b76119030bd
landrykapela <=>
2019-05-15 23:29:21 +0300

    Remove React defaults and add sections

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import Header from './Header.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <section className="App-section">
+      <div className="content">
+      </div>
+      </section>
+      <footer className="footer">
+      <div className="content">
+      </div>
+      </footer>
     </div>
   );
 }
